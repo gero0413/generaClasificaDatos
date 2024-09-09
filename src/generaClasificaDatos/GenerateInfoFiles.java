53b0c835603ea5ebaa93910d1ff54a0fba1ef982
RoldanPcLab <roldan0194@gmail.com>
2024-09-08 20:18:37 -0500

    Ajuste creacion de varios archivos vendedores

--- src/generaClasificaDatos/GenerateInfoFiles.java
+++ src/generaClasificaDatos/GenerateInfoFiles.java
@@ -2,13 +2,15 @@ package generaClasificaDatos;
 
 import java.io.*;
 import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GenerateInfoFiles {
 
     // Lista de nombres y apellidos para generar nombres de vendedores aleatorios
     private static final String[] nombres = {"Juan", "María", "Pedro", "Lucía", "Carlos", "Ana"};
-    private static final String[] apellidos = {"Pérez", "Gómez", "Rodríguez", "López", "Martínez", "García"};
-
+    private static final String[] apellidos = {"Pérez", "Gómez", "Rodríguez", "López", "Martínez", "García"}; 
+    
     // Método para generar un archivo de ventas de un vendedor
     public static void createSalesMenFile(int randomSalesCount, String name, long id) {
         String fileName = "ventas_" + id + ".txt";
@@ -44,8 +46,9 @@ public class GenerateInfoFiles {
     }
 
     // Método para generar un archivo con información de vendedores
-    public static void createSalesManInfoFile(int salesmanCount) {
+    public static Map<String, Long> createSalesManInfoFile(int salesmanCount) {
         String fileName = "informacion_vendedores.txt";
+        Map<String, Long> listSalesMan = new HashMap<>();
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             Random rand = new Random();
             for (int i = 0; i < salesmanCount; i++) {
@@ -53,18 +56,29 @@ public class GenerateInfoFiles {
                 String apellido = apellidos[rand.nextInt(apellidos.length)];
                 String tipoDocumento = "CC"; // Tipo de documento fijo
                 long numeroDocumento = rand.nextLong(100000000, 999999999); // Número aleatorio
-                writer.write(tipoDocumento + ";" + numeroDocumento + ";" + nombre + ";" + apellido + "\n");
+                writer.write(tipoDocumento + ";" + numeroDocumento + ";" + nombre + ";" + apellido + "\n"); //agregar linea al archivo plano
+                listSalesMan.put(nombre+" "+apellido,numeroDocumento); // agregar linea a la lista creada
             }
             System.out.println("Archivo de información de vendedores generado correctamente.");
         } catch (IOException e) {
             System.err.println("Error al crear el archivo de información de vendedores: " + e.getMessage());
         }
+        return listSalesMan;
     }
 
     public static void main(String[] args) {
+    	//Crear diccionario
+    	Map<String, Long> listSalesMan = new HashMap<>();
+    	
         // Prueba de los métodos para generar los archivos
-        createSalesMenFile(10, "Juan Pérez", 123456789);
         createProductsFile(50);
-        createSalesManInfoFile(5);
+        listSalesMan = createSalesManInfoFile(3);
+        
+        for (Map.Entry<String, Long> item : listSalesMan.entrySet()) {
+        	Long idSalesMan = item.getValue();
+        	String nameSalesMan = item.getKey();
+        	createSalesMenFile(10, nameSalesMan, idSalesMan);
+        }
+        
     }
 }
